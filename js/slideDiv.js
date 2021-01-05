371b552a77ef7d6b24fb4666b65bd1adbcca3014
Nelboh <eshobbs7@gmail.com>
2021-01-05 12:22:05 +0000

    Tinkered with slider page some more

--- js/slideDiv.js
+++ js/slideDiv.js
@@ -1,6 +1,28 @@
 let btn = document.getElementById('tabButton');
+let slider = document.getElementById('slider');
+
 let slidein = document.querySelector('.slidein');
 
-btn.addEventListener('click', function () {
-    slidein.classList.toggle('open');
+// [document.querySelector('.tabButton'), document.querySelector('.slider')].forEach(item => {
+//     item.addEventListener('click', event => {
+//         slidein.classList.toggle('open');
+//     })
+// })
+
+// btn.addEventListener('click', function () {
+//     slidein.classList.toggle('open');
+// });
+
+slider.addEventListener('click', function () {
+    slidein.classList.toggle('open')
 });
+
+// window.onload = function () {
+//     let divToHide = document.getElementById('divToHide');
+//     document.onclick = function (e) {
+//         if (e.target.id !== 'divToHide') {
+//             // if element clicked wasn't the div, hide it
+//             divToHide.classList.toggle('open');
+//         }
+//     };
+// };
